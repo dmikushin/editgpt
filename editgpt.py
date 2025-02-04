78facf156e4e9bbeda263a37a967a0f950219917
Dmitry Mikushin <dmitry@kernelgen.org>
2025-02-04 13:21:52 +0100

    Correcting the plugin code for the newer GEdit API

--- editgpt.py
+++ editgpt.py
@@ -1,49 +1,57 @@
-from gi.repository import GObject, Gedit, Gtk
+from gi.repository import GObject, Gedit, Gio, Gtk
 
-class MyPlugin(GObject.Object, Gedit.WindowActivatable):
-   __gtype_name__ = "MyPlugin"
-   window = GObject.Property(type=Gedit.Window)
+class EditGPTWindow(GObject.Object, Gedit.WindowActivatable):
+    window = GObject.Property(type=Gedit.Window)
 
-   def __init__(self):
-       super().__init__()
+    def __init__(self):
+        GObject.Object.__init__(self)
 
-   def do_activate(self):
-       self._insert_menu()
+    def do_activate(self):
+        self.action = Gio.SimpleAction(name="editgpt")
+        self.action.connect('activate', self.on_action_activate)
+        self.window.add_action(self.action)
 
-   def do_deactivate(self):
-       self._remove_menu()
+    def do_deactivate(self):
+        self.window.remove_action("editgpt")
 
-   def do_update_state(self):
-       pass
+    def on_action_activate(self, action, parameter, user_data=None):
+        dialog = Gtk.Dialog(title="Edit with ChatGPT", transient_for=self.window, flags=0)
+        dialog.add_buttons(Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
 
-   def _insert_menu(self):
-       action = Gtk.Action(name="OpenDialog", label="Open Dialog", tooltip="Open a dialog", stock_id=None)
-       action.connect("activate", self.on_action_activate)
-       action_group = Gtk.ActionGroup(name="MyPluginActions")
-       action_group.add_action_with_accel(action, "<Ctrl>G")
-       self.window.get_ui_manager().insert_action_group(action_group)
-       self._action_group = action_group
+        box = dialog.get_content_area()
+        text_view = Gtk.TextView()
+        text_view.set_wrap_mode(Gtk.WrapMode.WORD)
+        box.add(text_view)
 
-   def _remove_menu(self):
-       self.window.get_ui_manager().remove_action_group(self._action_group)
+        dialog.show_all()
+        response = dialog.run()
 
-   def on_action_activate(self, action):
-       dialog = Gtk.Dialog(title="My Dialog", transient_for=self.window, flags=0)
-       dialog.add_buttons(Gtk.STOCK_OK, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL)
+        if response == Gtk.ResponseType.OK:
+            buffer = text_view.get_buffer()
+            start_iter, end_iter = buffer.get_bounds()
+            text = buffer.get_text(start_iter, end_iter, True)
+            print("Text entered:", text)
 
-       box = dialog.get_content_area()
-       text_view = Gtk.TextView()
-       text_view.set_wrap_mode(Gtk.WrapMode.WORD)
-       box.add(text_view)
+        dialog.destroy()
 
-       dialog.show_all()
-       response = dialog.run()
+class EditGPTPlugin(GObject.Object, Gedit.AppActivatable):
+    __gtype_name__ = "EditGPTPlugin"
+    app = GObject.Property(type=Gedit.App)
 
-       if response == Gtk.ResponseType.OK:
-           buffer = text_view.get_buffer()
-           start_iter, end_iter = buffer.get_bounds()
-           text = buffer.get_text(start_iter, end_iter, True)
-           print("Text entered:", text)
+    def __init__(self):
+        GObject.Object.__init__(self)
 
-       dialog.destroy()
+    def do_activate(self):
+        self.app.set_accels_for_action("win.editgpt", [
+            "<Control>G"
+        ])
+        # self.menu_ext = self.extend_menu("tools-section")
+        # item = Gio.MenuItem.new("EditGPT", "win.editgpt")
+        # self.menu_ext.prepend_menu_item(item)
 
+    def do_deactivate(self):
+        self.app.set_accels_for_action("win.editgpt", [])
+        # self.menu_ext = None
+
+    def do_update_state(self):
+        pass
